6be35ac416aefff6a7add430ffa8a0523331a81e
hehaijin <hehaijin1981@gmail.com>
2019-11-25 20:40:18 -0700

    wapo again

--- wapo.js
+++ wapo.js
@@ -1,3 +1,4 @@
+document.getElementById('wallIframe').parentNode.parentNode.remove()
 document.getElementsByTagName('html')[0].style.overflow= 'auto'
 document.getElementsByTagName('body')[0].style.position= 'relative'
 document.getElementsByTagName('body')[0].style.height= 'auto'
